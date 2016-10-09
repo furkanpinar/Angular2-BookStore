b9c422cabe75dd0cdc5408abf39287744709b0f8
furkanpinar <frknpnr@gmail.com>
2016-10-09 11:12:42 +0300

    Shopping List Components Generate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,8 @@ import { BooksComponent } from './books/books.component';
 import { BookListComponent } from './books/book-list/book-list.component';
 import { BookItemComponent } from './books/book-list/book-item.component';
 import { BookDetailComponent } from './books/book-detail/book-detail.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingListAddComponent } from './shopping-list/shopping-list-add.component';
 
 @NgModule({
   declarations: [
@@ -17,7 +19,9 @@ import { BookDetailComponent } from './books/book-detail/book-detail.component';
     BooksComponent,
     BookListComponent,
     BookItemComponent,
-    BookDetailComponent
+    BookDetailComponent,
+    ShoppingListComponent,
+    ShoppingListAddComponent
   ],
   imports: [
     BrowserModule,
